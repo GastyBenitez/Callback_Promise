a5b215eb9528a3eb0c2b39c7d3bb29dd6af426b6
gaston benitez <gastonbenitez0207@gmail.com>
2022-05-10 21:01:13 -0300

    inicio clase

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// FUNCIONAMIENTO DE UN CALLBACK
+const meses = ["Enero", "Febrero", "Marzo", "Abril",]
+
+function nuevoMes (mes,callback) {
+    setTimeout(()=>{
+        meses.push(mes)
+    
+        callback()
+    }, 3000)
+    
+}
+
+
+
+function mostrarMeses () {
+    setTimeout(()=>{
+        meses.forEach(mes => { console.log(mes) })
+        console.log("---------------------");
+    },3000)
+    
+
+}
+
+mostrarMeses()
+nuevoMes("Mayo", mostrarMeses)
