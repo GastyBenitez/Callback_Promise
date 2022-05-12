7063475e559e879a67d0064c095a8e120bf51e26
gaston benitez <gastonbenitez0207@gmail.com>
2022-05-12 19:32:41 -0300

    tarea terminada

--- js/app2.js
+++ js/app2.js
@@ -1,18 +1,28 @@
+const mensaje = document.querySelector("#body");
+
 const aplicarDescuento = new Promise((resolve, reject)=>{
     const dto = false
     if(dto){
-        resolve("Descuento aplicado")
+        resolve("Descuento aplicado.")
     }else{
-        reject("Descuento no aplicado")
+        reject("Descuento no aplicado.")
     }
 });
 
 aplicarDescuento
     .then(res =>{
         console.log(res);
+    const mensajeP = document.createElement("p");
+    mensajeP.classList.add("aplicado")
+    mensajeP.textContent =  res
+    mensaje.appendChild(mensajeP);
 })
 .catch(error=>{
-    console.error(error);
+    console.warn(error);
+    const mensajeP2 = document.createElement("p");
+    mensajeP2.classList.add("no_aplicado")
+    mensajeP2.textContent = error
+    mensaje.appendChild(mensajeP2)
 })
 
 // console.log(aplicarDescuento);
@@ -25,3 +35,5 @@ rejected -> CUANDO LA PROMESA NO SE CUMPLIÓ
 pendding -> CUANDO QUEDA PENDIENTE
 
 */
+
+//CREAR CON JAVASCRIPT UN CODIGO PARA QUE SE PUEDA MOSTRAR EN EL DOM LA INFO QUE DEVULVE LA PROMESA
